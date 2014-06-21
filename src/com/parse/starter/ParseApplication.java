0199e50eb59e1765ec0a253562259df1711a9855
HiceS <shawnhice@gmail.com>
2014-06-20 18:59:51 -0700

    Fixed all tables and correctly formated Date

--- src/com/parse/starter/ParseApplication.java
+++ src/com/parse/starter/ParseApplication.java
@@ -21,16 +21,12 @@ public class ParseApplication extends Application {
 		
 		//Data Forms section 
 		
-		//Install table information
-		ParseObject install_data = new ParseObject("Install Data");
-		ParseObject ticket_data = new ParseObject("Ticket Data");
-		ParseObject user_table = new ParseObject("User Data");
-		ParseObject system_table = new ParseObject("System Data");
-		ParseObject drawing_table = new ParseObject("Drawing Data");
-		Calendar install_date = Calendar.getInstance();
-		
 		//The getters for the data
-		int date = install_date.DATE;
+		Calendar c = Calendar.getInstance(); 
+        	int day = c.get(Calendar.DATE);
+        	int month = c.get(Calendar.MONTH);
+        	int year = c.get(Calendar.YEAR);
+        	String date = month + "-" + day + "-" + year;
 		String android_id = Secure.getString(getBaseContext().getContentResolver(),
                 Secure.ANDROID_ID);
 		double app_version = 1.0;
@@ -50,45 +46,50 @@ public class ParseApplication extends Application {
 		int distribution_tickets = 0;
 		int graded_tickets = 0;
 		
-		//Putting the data into the table 
-		install_data.put("Date", date);
-		install_data.put("Phone information", android_id);
-		install_data.put("App version", app_version);
-		install_data.put("Username", username_getter);
+		//Putting the data into the installation table 
+		ParseObject install_data = new ParseObject("Installation");
+		install_data.put("date", date);
+		install_data.put("phoneID", android_id);
+		install_data.put("app_version", app_version);
+		install_data.put("username", username_getter);
 		install_data.saveInBackground();
 		
-		//Putting the data into the table 
-		ticket_data.put("Ticket Number", ticket_number);
-		ticket_data.put("Username", username_getter);
-		ticket_data.put("Date", date);
-		ticket_data.put("Matched Numbers", matchedNumbers);
-		ticket_data.put("Prize", prize);
+		//Putting the data into the ticket info table 
+		ParseObject ticket_data = new ParseObject("Ticket");
+		ticket_data.put("ticket_Number", ticket_number);
+		ticket_data.put("username", username_getter);
+		ticket_data.put("date", date);
+		ticket_data.put("matched_Numbers", matchedNumbers);
+		ticket_data.put("prize", prize);
 		ticket_data.saveInBackground();		
 		
-		//Putting the data into the table 
-		user_table.put("Current Ticket Number", ticket_number);
-		user_table.put("Username", username_getter);
-		user_table.put("Created Date", date);
-		user_table.put("Email", account_email);
-		user_table.put("Facebook", facebook_info);
-		user_table.put("Twitter", twitter_info);
-		user_table.put("Picture", prize);
+		//Putting the data into the user info table 
+		ParseObject user_table = new ParseObject("User");
+		user_table.put("current_Ticket_Number", ticket_number);
+		user_table.put("username", username_getter);
+		user_table.put("created_Date", date);
+		user_table.put("email", account_email);
+		user_table.put("facebook", facebook_info);
+		user_table.put("twitter", twitter_info);
+		user_table.put("picture", prize);
 		user_table.saveInBackground();
 		
-		//Putting the data into the table 
-		system_table.put("Current Jackpot Value", ticket_number);
-		system_table.put("Next Drawing Time", nextDrawingTime);
-		system_table.put("Next Sweepstakes Time", nextSweepstakesTime);
-		system_table.put("Jackpot Entries", jackpotEntries);
-		system_table.put("Sweepstakes Entries", sweepstake_entries);
+		//Putting the data into the system table 
+		ParseObject system_table = new ParseObject("System");
+		system_table.put("current_Jackpot_Value", jackpot_value);
+		system_table.put("next_Drawing_Time", nextDrawingTime);
+		system_table.put("next_Sweepstakes_Time", nextSweepstakesTime);
+		system_table.put("jackpot_Entries", jackpotEntries);
+		system_table.put("sweepstakes_Entries", sweepstake_entries);
 		system_table.saveInBackground();
 		
-		//Putting the data into the table 
-		drawing_table.put("Date of Drawing", date);
-		drawing_table.put("Time of Drawing", date);
-		drawing_table.put("Winning Numbers Generated", winningNumbers);
-		drawing_table.put("Distribution of ticket results", distribution_tickets);
-		drawing_table.put("Graded Tickets", graded_tickets);
+		//Putting the data into the drawing table 
+		ParseObject drawing_table = new ParseObject("Drawing");
+		drawing_table.put("date_of_Drawing", date);
+		drawing_table.put("time_of_Drawing", date);
+		drawing_table.put("winning_Numbers_Generated", winningNumbers);
+		drawing_table.put("distribution_of_ticket_results", distribution_tickets);
+		drawing_table.put("graded_Tickets", graded_tickets);
 		drawing_table.saveInBackground();
 		
 		//Parse user default area
