0395c7659fa7509dcaebe2d3941b8e63f212e097
HiceS <shawnhice@gmail.com>
2014-06-28 15:22:15 -0700

    Added Simple setters and getters for user
    I am conflicted between working on the cloud code to generate the
    winning numbers and writing a query to find them with the local user or
    keep developing app until i need to do something else.

--- src/com/parse/starter/PlayScreen.java
+++ src/com/parse/starter/PlayScreen.java
@@ -1,18 +1,18 @@
 package com.parse.starter;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
+
+import com.parse.ParseUser;
 
 public class PlayScreen extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.play_screen);
+		
+		ParseUser current = ParseUser.getCurrentUser();
+		Boolean isnew = current.isNew();
+		String username = current.getUsername();
+		
 	}
 }
