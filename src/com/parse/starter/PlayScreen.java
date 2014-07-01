b525dbf0907ac972a9f025df52bf60530751bf72
HiceS <shawnhice@gmail.com>
2014-07-01 13:29:09 -0700

    Finally added input from cloud data .js file
    Im having a bit of trouble publishing or deploying the new code i wrote
    for the cloud service but ill be working on that today. Also i will be
    trying to get the random generated numbers working as i go along.

--- src/com/parse/starter/PlayScreen.java
+++ src/com/parse/starter/PlayScreen.java
@@ -1,8 +1,17 @@
 package com.parse.starter;
 
-import android.app.Activity;
-import android.os.Bundle;
+import java.util.HashMap;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+
+import com.parse.FunctionCallback;
+import com.parse.ParseCloud;
+import com.parse.ParseException;
 import com.parse.ParseUser;
 
 public class PlayScreen extends Activity {
@@ -13,6 +22,22 @@ public class PlayScreen extends Activity {
 		ParseUser current = ParseUser.getCurrentUser();
 		Boolean isnew = current.isNew();
 		String username = current.getUsername();
+		
+		TextView result = (TextView) findViewById(R.id.result);
+		
+		ParseCloud.callFunctionInBackground("hello", new HashMap<String, Object>(), new FunctionCallback<String>() {
+			  public void done(String result, ParseException e) {
+			    if (e == null) {
+			      // result is "Hello world!"
+			    	AlertDialog.Builder alert = new AlertDialog.Builder(
+			    			PlayScreen.this);
+					alert.setTitle("works");
+					alert.setMessage(result);
+					alert.show();
+			    	
+			    }
+			  }
+			});
 		
 	}
 }
